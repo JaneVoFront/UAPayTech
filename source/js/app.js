fb26554a88e0f1a89972999033e9fb8afcc11eda
Jane Vo <alterxru@gmail.com>
2021-08-03 00:59:15 +0300

    104

--- source/js/app.js
+++ source/js/app.js
@@ -17,17 +17,17 @@ var x = new MyClass(cred)
 console.log(x.getName())
 
 //Tabs
-let tabs = document.querySelector('.tabs')
-let mitem = tabs.querySelectorAll('.tabs-menu__item')
-let citem = tabs.querySelectorAll('.tabs-content__item')
-function change(arr, i) {
-  arr.forEach( item => {
-    item.forEach( i => {i.classList.remove('is-active')})
-    item[i].classList.add('is-active')
-  })
-}
-for(let i = 0; i < mitem.length; i++) {
-  mitem[i].addEventListener('click', () => {
-    change([mitem, citem], i)
-  })
-}
+// let tabs = document.querySelector('.tabs')
+// let mitem = tabs.querySelectorAll('.tabs-menu__item')
+// let citem = tabs.querySelectorAll('.tabs-content__item')
+// function change(arr, i) {
+//   arr.forEach( item => {
+//     item.forEach( i => {i.classList.remove('is-active')})
+//     item[i].classList.add('is-active')
+//   })
+// }
+// for(let i = 0; i < mitem.length; i++) {
+//   mitem[i].addEventListener('click', () => {
+//     change([mitem, citem], i)
+//   })
+// }
--- source/js/tabs.js
+++ source/js/tabs.js
@@ -1,16 +1,20 @@
-let tabs = document.querySelector('.tabs')
-let    mitem = tabs.querySelectorAll('.tabs-menu__item')
-let   citem = tabs.querySelectorAll('.tabs-content__item')
+window.onload = function() {
+    let tabs = document.querySelector('.tabs')
+    let mitem = tabs.querySelectorAll('.tabs-menu__item')
+    let citem = tabs.querySelectorAll('.tabs-content__item')
 
-function change(arr, i) {
-    arr.forEach( item => {
-        item.forEach( i => {i.classList.remove('is-active')})
-        item[i].classList.add('is-active')
-    })
-}
+    function change(arr, i) {
+        arr.forEach(item => {
+            item.forEach(i => {
+                i.classList.remove('is-active')
+            })
+            item[i].classList.add('is-active')
+        })
+    }
 
-for(let i = 0; i < mitem.length; i++) {
-    mitem[i].addEventListener('click', () => {
-        change([mitem, citem], i)
-    })
+    for (let i = 0; i < mitem.length; i++) {
+        mitem[i].addEventListener('click', () => {
+            change([mitem, citem], i)
+        })
+    }
 }
